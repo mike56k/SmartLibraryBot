393b5e557957c15327474395e4051ed065715581
Rodion Ibragimov <rodion.ibragimov@red-soft.ru>
2025-07-31 22:10:33 +0300

    feat: hot reload

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,7 @@
 import asyncio
+import logging
+import os
+import sys
 
 from application.book import return_book
 from application.button import handle_buttons
@@ -10,19 +13,51 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+logger = logging.getLogger("bot")
+
+
+class ReloadHandler(FileSystemEventHandler):
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self.loop = loop
+
+    def on_any_event(self, event: FileSystemEvent):
+        logger.info("Изменение обнаружено, перезапускаем бота...")
+        self.loop.call_soon_threadsafe(self.restart)
+
+    def restart(self):
+        logger.info("Перезапуск процесса...")
+        python = sys.executable
+        os.execv(python, [python] + sys.argv)
 
 
 def main():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    loop.run_until_complete(settings.PUNISHMENT_SYSTEM_SERVICE.start())
+    observer = Observer()
+    handler = ReloadHandler(loop)
+    observer.schedule(handler, path=".", recursive=True)  # отслеживаем папку проекта
+    observer.start()
 
-    settings.APP.add_handler(CommandHandler("start", start))
-    settings.APP.add_handler(CallbackQueryHandler(handle_buttons))
-    settings.APP.add_handler(MessageHandler(filters.Document.FileExtension("pdf"), return_book))
+    try:
+        # Запускаете бота (или ваши задачи, например, polling)
+        loop.run_until_complete(settings.PUNISHMENT_SYSTEM_SERVICE.start())
+        logger.info("БОТИНОК ЗАПУЩЕНОМАНЭ")
 
-    settings.APP.run_polling()
+        settings.APP.add_handler(CommandHandler("start", start))
+        settings.APP.add_handler(CallbackQueryHandler(handle_buttons))
+        settings.APP.add_handler(MessageHandler(filters.Document.FileExtension("pdf"), return_book))
+
+        settings.APP.run_polling()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        observer.stop()
+        observer.join()
 
 
 if __name__ == "__main__":
