2490a2c785551701fee346797a62277bb423c8dc
Rodion Ibragimov <rodion.ibragimov@red-soft.ru>
2025-08-11 19:42:13 +0300

    feat: docker

--- bot/main.py
+++ bot/main.py
@@ -4,10 +4,6 @@ import os
 import sys
 from pathlib import Path
 
-from application.book import return_book
-from application.button import handle_buttons
-from application.starter import start
-from core.settings import settings
 from telegram.ext import (
     CallbackQueryHandler,
     CommandHandler,
@@ -17,6 +13,12 @@ from telegram.ext import (
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
+from application.book import return_book
+from application.button import handle_buttons
+from application.starter import start
+from core.settings import settings
+from resources.start_bot_text import start_bot_text
+
 logger = logging.getLogger("bot")
 
 
@@ -24,6 +26,7 @@ class ReloadHandler(FileSystemEventHandler):
     EXCLUDE_PATHS: list[str] = [
         settings.BORROWED_DATA_FILE.relative_to(settings.BASE_PATH).as_posix(),
         Path(settings.BOOKS_DIR).relative_to(settings.BASE_PATH.parent).as_posix(),
+        Path(settings.LOG_FILE).relative_to(settings.BASE_PATH.parent).as_posix(),
         "__pycache__",
     ]
 
@@ -32,6 +35,7 @@ class ReloadHandler(FileSystemEventHandler):
 
     def on_any_event(self, event: FileSystemEvent):
         if not any(excluded in event.src_path for excluded in self.EXCLUDE_PATHS):
+            logger.info(event.src_path)
             logger.info("Изменение обнаружено, перезапускаем бота...")
             self.loop.call_soon_threadsafe(self.restart)
 
@@ -53,8 +57,7 @@ def main():
     try:
         # Запускаете бота (или ваши задачи, например, polling)
         loop.run_until_complete(settings.PUNISHMENT_SYSTEM_SERVICE.start())
-        logger.info("БОТИНОК ЗАПУЩЕНОМАНЭ")
-        logger.info("РАСПЕРЖИВАЭ ЭТУ ШАХУ")
+        logger.info(start_bot_text)
         settings.APP.add_handler(CommandHandler("start", start))
         settings.APP.add_handler(CallbackQueryHandler(handle_buttons))
         settings.APP.add_handler(MessageHandler(filters.Document.FileExtension("pdf"), return_book))
